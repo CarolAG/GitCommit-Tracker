210ac00904afe5723d2c31dc0bab82a1a9ebc600
Alex Zai <azai91@gmail.com>
2015-10-25 00:20:03 -0700

    (Feat) replace boilerplate code

--- client/app.js
+++ client/app.js
@@ -12,23 +12,23 @@ var app = angular
 // we pass in a function that handles our routes and config to the config function on that same angular module 'myApp' that we created above
 //
 // NOTE: we do not redeclare our external dependencies, we only need to do this once
-// app.config(configFunction);
+app.config(configFunction);
 
-// // we $inject any dependencies that that function will need
-// // configFunction.$inject = ['$routeProvider', '$locationProvider'];
+// we $inject any dependencies that that function will need
+// configFunction.$inject = ['$routeProvider', '$locationProvider'];
 
-// // make sure these arguments are ordered the same as the ones in your $inject
-// function configFunction($routeProvider, $locationProvider) {
+// make sure these arguments are ordered the same as the ones in your $inject
+function configFunction($routeProvider, $locationProvider) {
 
-//   $routeProvider
-//     .when('/', {
-//       // templateUrl: './partials/home.html',
-//       controller: 'HomeController'
-//     })
-//     // to remove
-//     .when('/about', {
-//       // templateUrl: './partials/about.html',
-//       controller: 'AboutController'
-//     });
-// }
+  $routeProvider
+    .when('/', {
+      templateUrl: './partials/home.html',
+      controller: 'HomeController'
+    })
+    // to remove
+    .when('/about', {
+      // templateUrl: './partials/about.html',
+      controller: 'AboutController'
+    });
+}
 
