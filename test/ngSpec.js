c58ad2e05b94389605fd0650f7930ef5b0a34191
Andrew Carlson <acarl005@g.ucla.edu>
2016-01-14 15:33:14 -0800

    fix oup the formatting

--- test/ngSpec.js
+++ test/ngSpec.js
@@ -41,7 +41,7 @@ describe('Angular Spec', function() {
 
     it('AboutController controller should be created', function() {
       var $scope = {};
-      expect(controller).withArgs('AboutController', {$scope: $scope}).to.not.throwException();
+      expect(controller).withArgs('AboutController', { $scope: $scope }).to.not.throwException();
     });
 
     it('AboutController should have "name" property', function() {
@@ -53,51 +53,83 @@ describe('Angular Spec', function() {
 
   });
 
-  // PENDING
-  xdescribe('Factories',function() {
+  describe('UserFactory', function() {
 
     beforeEach(function() {
-      try {
-        module('myApp');
-      } catch(e) {
-        console.log('there');
-      }
+      module('myApp');
     });
-    var MockUserFactory;
+    var MockUserFactory = { name: 'P!NK', age: 9001 };
 
-    it('description', function() {
-     try {
-      expect(inject).withArgs(function ($controller, UserFactory) {
+    it('UserFactory should be defined', function() {
+      expect(inject).withArgs(function($controller, UserFactory) {
         controller = $controller;
-        MockUserFactory = UserFactory;
-      }).to.throwException();
-      } catch(e) {
-        console.log('shit');
-      }
-
+      }).to.not.throwException();
     });
 
-    beforeEach(inject(function ($controller, UserFactory) {
-      controller = $controller;
-      MockUserFactory = UserFactory;
-    }));
+    it('UserFactory should have name and age properties', function() {
+      inject(function(UserFactory) {
+        expect(UserFactory.name).to.be.a('string');
+        expect(UserFactory.age).to.be.a('number');
+      });
+    });
 
     it('UserFactory should be injected into the HomeController', function() {
       var $scope = {};
-      expect(controller).withArgs('HomeController', {$scope: $scope, UserFactory: MockUserFactory}).to.throwException();
-      // expect($scope.name).to.be.a('string');
-      // expect($scope.name).to.be.truthy;
+      controller('HomeController', {
+        $scope: $scope,
+        UserFactory: MockUserFactory
+      });
+      expect($scope.name).to.eql('P!NK');
+    });
+
+    it('AboutController should have method that updates the UserFactory', function() {
+      var $scope = {};
+      controller('AboutController', {
+        $scope: $scope,
+        UserFactory: MockUserFactory
+      });
+      expect($scope.name).to.eql('P!NK');
+      $scope.name = 'GR33N';
+      $scope.age = 1337;
+      $scope.save();
+      expect(MockUserFactory.name).to.equal('GR33N');
+      expect(MockUserFactory.age).to.equal(1337);
     });
 
  });
 
-  describe('HTTP', function() {
-    it('HomeController should have "getMessages" method', function() {
-      var $scope = {};
-      HomeController = controller('HomeController', { $scope: $scope });
-      expect($scope.getMessages).to.be.a('function');
-      expect($scope.getMessages).to.be.truthy;
-    });
-  });
-});
+  describe('MessageFactory', function() {
 
+    beforeEach(function() {
+      module('myApp');
+    });
+    var TheMessageFactory;
+    var promiseConstructor;
+
+
+    it('MessageFactory should be defined', function(done) {
+      expect(inject).withArgs(function($controller, MessageFactory, $http) {
+        promiseConstructor = $http.get().constructor;
+        controller = $controller;
+        TheMessageFactory = MessageFactory;
+        done();
+      }).to.not.throwException();
+    });
+
+    it('MessageFactory should have "fetch" method and "post"', function() {
+      expect(TheMessageFactory.fetch).to.be.a('function');
+    });
+
+    it('MessageFactory "fetch" method should get messages from the project Slack server and return a promise', function(done) {
+      expect(TheMessageFactory.fetch().constructor).to.eql(promiseConstructor);
+      done();
+    });
+
+    it('MessageFactory "fetch" method should post messages to the project Slack server and return a promise', function(done) {
+      expect(TheMessageFactory.post().constructor).to.eql(promiseConstructor);
+      done();
+    });
+
+  });
+
+});
